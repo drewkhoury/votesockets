60ab14d1631fe42abd28ae021788340c457fc053
Drew Khoury <drew.khoury@gmail.com>
2013-06-27 17:17:28 +1000

    pushing port 80 for aws opsworks

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var fs = require('fs')
     , http = require('http')
     , socketio = require('socket.io');
 
-// for heroku
-var port = process.env.PORT || 5000;
+// for aws
+var port = 80;
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
